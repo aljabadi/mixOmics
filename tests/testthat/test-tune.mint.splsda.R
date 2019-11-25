6ab3e6266759e2deaf9e0414faa4ad99df1fe7a4
Al J Abadi <al.jal.abadi@gmail.com>
2019-11-25 16:54:10 +1100

    update tune.mint.splsda tests

--- tests/testthat/test-tune.mint.splsda.R
+++ tests/testthat/test-tune.mint.splsda.R
@@ -5,21 +5,24 @@ test_that("tune.mint.splsda works", code = {
     data = stemcells$gene
     type.id = stemcells$celltype
     exp = stemcells$study
+    test.keepX = c(20, 40)
     
+    RNGversion(.mixo_rng())
+    
+    set.seed(42)
     out = tune.mint.splsda(
         X = data,
         Y = type.id,
         ncomp = 2,
         near.zero.var = FALSE,
         study = exp,
-        test.keepX = seq(1, 5, 2)
+        test.keepX = test.keepX
     )
-    
-    out$choice.ncomp
-    out$choice.keepX
     expect_is(out, "tune.mint.splsda")
     expect_equal(out$choice.ncomp$ncomp, 1)
-    
+    errs <- signif(unname(out$error.rate[,1]), 2)
+    dput(errs)
+    expect_equal(errs, c(0.39, 0.39))
 })
 
 test_that("tune.mint.splsda works with custom alpha", code = {
@@ -27,20 +30,22 @@ test_that("tune.mint.splsda works with custom alpha", code = {
     data = stemcells$gene
     type.id = stemcells$celltype
     exp = stemcells$study
+    test.keepX = c(20, 40)
     
+    RNGversion(.mixo_rng())
+    
+    set.seed(42)
     out = tune.mint.splsda(
         X = data,
         Y = type.id,
         ncomp = 2,
         near.zero.var = FALSE,
         study = exp,
-        test.keepX = seq(1, 5, 2),
-        signif.threshold = 0.05
+        test.keepX = test.keepX,
+        signif.threshold = 0.9
     )
-    
-    out$choice.ncomp
-    out$choice.keepX
     expect_is(out, "tune.mint.splsda")
-    expect_equal(out$choice.ncomp$ncomp, 1)
-    
+    errs <- signif(unname(out$error.rate[,1]), 2)
+    expect_equal(errs, c(0.39, 0.39))
+    expect_equal(out$choice.ncomp$ncomp, 2)
 })
