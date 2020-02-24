c1280480826c37ba4e5a003b980578eeb3dea2c1
Al J Abadi <al.jal.abadi@gmail.com>
2020-02-24 16:49:35 +1100

    change nipals function

--- R/nipals.R
+++ R/nipals.R
@@ -25,136 +25,137 @@
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #############################################################################################################
 
-nipals = function (X, ncomp = 1, reconst = FALSE, max.iter = 500, tol = 1e-09)
+nipals = function (X, ncomp = 1, reconst = FALSE, max.iter = 500, tol = 1e-09,
+                   center = FALSE, scale = FALSE,  startcol = 0,
+                   force.na = TRUE, gramschmidt = FALSE, verbose = FALSE)
 {
-    
-    #-- X matrix
-    if (is.data.frame(X))
-    X = as.matrix(X)
-    
-    if (!is.matrix(X) || is.character(X))
-    stop("'X' must be a numeric matrix.", call. = FALSE)
-    
-    if (any(apply(X, 1, is.infinite)))
-    stop("infinite values in 'X'.", call. = FALSE)
-    
-    nc = ncol(X)
-    nr = nrow(X)
-    #-- put a names on the rows and columns of X --#
-    X.names = colnames(X)
-    if (is.null(X.names))
-    X.names = paste("V", 1:ncol(X), sep = "")
-    
-    ind.names = rownames(X)
-    if (is.null(ind.names))
-    ind.names = 1:nrow(X)
-    
-    #-- ncomp
-    if (is.null(ncomp) || !is.numeric(ncomp) || ncomp < 1 || !is.finite(ncomp) || ncomp>min(nr,nc))
-    stop("invalid value for 'ncomp'.", call. = FALSE)
-    
-    ncomp = round(ncomp)
-    
-    #-- reconst
-    if (!is.logical(reconst))
-    stop("'reconst' must be a logical constant (TRUE or FALSE).",
-    call. = FALSE)
-    
-    #-- max.iter
-    if (is.null(max.iter) || max.iter < 1 || !is.finite(max.iter))
-    stop("invalid value for 'max.iter'.", call. = FALSE)
-    
-    max.iter = round(max.iter)
-    
-    #-- tol
-    if (is.null(tol) || tol < 0 || !is.finite(tol))
-    stop("invalid value for 'tol'.", call. = FALSE)
-    
-    #-- end checking --#
-    #------------------#
-    #-- pca approach -----------------------------------------------------------#
-    #---------------------------------------------------------------------------#
-    
-    
-    #-- initialisation des matrices --#
-    p = matrix(nrow = nc, ncol = ncomp)
-    t.mat = matrix(nrow = nr, ncol = ncomp)
-    eig = vector("numeric", length = ncomp)
-    nc.ones = rep(1, nc)
-    nr.ones = rep(1, nr)
-    is.na.X = is.na(X)
-    na.X = FALSE
-    if (any(is.na.X)) na.X = TRUE
-    
-    #-- boucle sur h --#
-    for (h in 1:ncomp)
-    {
-        th = X[, which.max(apply(X, 2, var, na.rm = TRUE))]
-        if (any(is.na(th))) th[is.na(th)] = 0
-        ph.old = rep(1 / sqrt(nc), nc)
-        ph.new = vector("numeric", length = nc)
-        iter = 1
-        diff = 1
-        
-        if (na.X)
-        {
-            X.aux = X
-            X.aux[is.na.X] = 0
-        }
-        
-        while (diff > tol & iter <= max.iter)
-        {
-            if (na.X)
-            {
-                ph.new = crossprod(X.aux, th)
-                Th = drop(th) %o% nc.ones
-                Th[is.na.X] = 0
-                th.cross = crossprod(Th)
-                ph.new = ph.new / diag(th.cross)
-            } else {
-                ph.new = crossprod(X, th) / drop(crossprod(th))
-            }
-            
-            ph.new = ph.new / drop(sqrt(crossprod(ph.new)))
-            
-            if (na.X)
-            {
-                th = X.aux %*% ph.new
-                P = drop(ph.new) %o% nr.ones
-                P[t(is.na.X)] = 0
-                ph.cross = crossprod(P)
-                th = th / diag(ph.cross)
-            } else {
-                th = X %*% ph.new / drop(crossprod(ph.new))
-            }
-            
-            diff = drop(sum((ph.new - ph.old)^2, na.rm = TRUE))
-            ph.old = ph.new
-            iter = iter + 1
-        }
-        
-        if (iter > max.iter)
-        warning(paste("Maximum number of iterations reached for comp.", h))
-        
-        X = X - th %*% t(ph.new)
-        p[, h] = ph.new
-        t.mat[, h] = th
-        eig[h] = sum(th * th, na.rm = TRUE)
+    ## proudly taken from the mighty nipals::nipals
+    x <- as.matrix(X)
+    nvar <- ncol(x)
+    nobs <- nrow(x)
+    x.orig <- x
+    col.na.count <- apply(x, 2, function(x) sum(!is.na(x)))
+    if (any(col.na.count == 0)) 
+        stop("At least one column is all NAs")
+    row.na.count <- apply(x, 1, function(x) sum(!is.na(x)))
+    if (any(row.na.count == 0)) 
+        stop("At least one row is all NAs")
+    if (center) {
+        cmeans <- colMeans(x, na.rm = TRUE)
+        x <- sweep(x, 2, cmeans, "-")
     }
-    
+    else cmeans <- NA
+    if (scale) {
+        csds <- apply(x, 2, sd, na.rm = TRUE)
+        x <- sweep(x, 2, csds, "/")
+    }
+    else csds <- NA
+    TotalSS <- sum(x * x, na.rm = TRUE)
+    PPp = matrix(0, nrow = nvar, ncol = nvar)
+    TTp = matrix(0, nrow = nobs, ncol = nobs)
+    eig <- rep(NA, length = ncomp)
+    R2cum <- rep(NA, length = ncomp)
+    loadings <- matrix(nrow = nvar, ncol = ncomp)
+    scores <- matrix(nrow = nobs, ncol = ncomp)
+    iter <- rep(NA, length = ncomp)
+    x.miss <- is.na(x)
+    has.na <- any(x.miss)
+    if (force.na) 
+        has.na <- TRUE
+    for (h in 1:ncomp) {
+        if (is.function(startcol)) {
+            scol <- which.max(apply(x, 2, startcol))
+        }
+        else if (startcol == 0L) {
+            scol <- which.max(apply(x, 2, function(x) sum(abs(x), 
+                                                          na.rm = TRUE)))
+        }
+        else {
+            scol <- startcol
+        }
+        if (verbose >= 1) 
+            cat("PC ", h, " starting column: ", scol, sep = "")
+        if (has.na) {
+            x0 <- x
+            x0[x.miss] <- 0
+            th <- x0[, scol]
+        }
+        else {
+            th <- x[, scol]
+        }
+        pciter <- 1
+        continue <- TRUE
+        while (continue) {
+            if (has.na) {
+                T2 <- matrix(th * th, nrow = nobs, ncol = nvar)
+                T2[x.miss] <- 0
+                ph <- crossprod(x0, th)/colSums(T2)
+            }
+            else {
+                ph = crossprod(x, th)/sum(th * th)
+            }
+            if (gramschmidt && h > 1) {
+                ph <- ph - PPp %*% ph
+            }
+            ph <- ph/sqrt(sum(ph * ph, na.rm = TRUE))
+            th.old <- th
+            if (has.na) {
+                P2 <- matrix(ph * ph, nrow = nvar, ncol = nobs)
+                P2[t(x.miss)] <- 0
+                th = x0 %*% ph/colSums(P2)
+            }
+            else {
+                th = x %*% ph/sum(ph * ph)
+            }
+            if (gramschmidt && h > 1) {
+                th <- th - TTp %*% th
+            }
+            if (sum((th - th.old)^2, na.rm = TRUE) < tol) 
+                continue = FALSE
+            pciter <- pciter + 1
+            if (pciter == max.iter) {
+                continue <- FALSE
+                warning("Stopping after ", max.iter, " iterations for PC ", 
+                        h, ".\n")
+            }
+            if (verbose >= 1) 
+                cat(".")
+        }
+        if (verbose >= 1) 
+            cat("\n")
+        x <- x - (th %*% t(ph))
+        loadings[, h] <- ph
+        scores[, h] <- th
+        eig[h] <- sum(th * th, na.rm = TRUE)
+        iter[h] <- pciter
+        if (gramschmidt) {
+            PPp = PPp + tcrossprod(ph)
+            TTp = TTp + tcrossprod(th)/eig[h]
+        }
+        R2cum[h] <- 1 - (sum(x * x, na.rm = TRUE)/TotalSS)
+    }
+    R2 <- c(R2cum[1], diff(R2cum))
     eig = sqrt(eig)
-    t.mat = scale(t.mat, center = FALSE, scale = eig)
-    attr(t.mat, "scaled:scale") = NULL
-    result = list(eig = eig, p = p, t = t.mat)
-    
-    if (reconst)
-    {
-        X.hat = t.mat %*% diag(eig) %*% t(p)
-        
-        colnames(X.hat) = colnames(X)
-        rownames(X.hat) = rownames(X)
-        result$rec = X.hat
+    scores = sweep(scores, 2, eig, "/")
+    if (reconst) {
+        xhat <- tcrossprod(sweep(scores, 2, eig, "*"), loadings)
+        if (scale) 
+            xhat <- sweep(xhat, 2, csds, "*")
+        if (center) 
+            xhat <- sweep(xhat, 2, cmeans, "+")
+        rownames(xhat) <- rownames(x.orig)
+        colnames(xhat) <- colnames(x.orig)
     }
-    
-    return(invisible(result))
+    else {
+        xhat <- NULL
+    }
+    rownames(scores) <- rownames(x)
+    colnames(scores) <- paste("PC", 1:ncol(scores), sep = "")
+    rownames(loadings) <- colnames(x)
+    colnames(loadings) <- paste("PC", 1:ncol(loadings), sep = "")
+    out <- list(eig = eig, t = scores, p = loadings)
+    if (reconst) {
+        out$rec <- xhat
+    }
+    return(out)
 }
