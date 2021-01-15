c6dd81561325cbecc0ed101cc0ec5e4df2cc914e
Al J Abadi <al.jal.abadi@gmail.com>
2021-01-15 16:23:15 +1100

    why this run

--- buildignore/devel/merge-devel.R
+++ buildignore/devel/merge-devel.R
@@ -7,3 +7,11 @@
 ## we should also increase the examples / unit tests
 ## then we can push to orihin and upstream
 ## we can also modularise the handbook changes
+list.keepX <- c(2:10, 15, 20)
+# tuning based on correlations
+set.seed(30) # for reproducbility in this vignette, otherwise increase nrepeat
+tune.spls.cor <- tune.spls(X, Y, ncomp = 3,
+                           test.keepX = list.keepX,
+                           validation = "Mfold", folds = 5,
+                           nrepeat = 10, progressBar = FALSE,
+                           measure = 'cor')
