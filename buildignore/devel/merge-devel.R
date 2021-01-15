3e25c18278cd191d6d0fdda0ff97cff0084c2238
Al J Abadi <al.jal.abadi@gmail.com>
2021-01-15 16:22:45 +1100

    now

--- buildignore/devel/merge-devel.R
+++ buildignore/devel/merge-devel.R
@@ -1,1 +1,9 @@
 ## 
+## vignette build is breaking
+## it is due to changes to devel and Handbook
+## the Hanbook changes did create a successful handbook build
+## but it seems like the same changes are breaking the vignette
+## we should gather all these changes and build the Handbook and the package at the same time on every change
+## we should also increase the examples / unit tests
+## then we can push to orihin and upstream
+## we can also modularise the handbook changes
